1bcd6a2c371d1a16ba18c88db57171f37391a9c9
wuye <ye.wu@ppiaas.cn>
2021-12-16 11:21:07 +0800

    66 plus one

---
+++ leet_code/66_plus_one/solution.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=golang
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+
+func plusOne(digits []int) []int {
+	var idx = len(digits) - 1
+	for {
+		if digits[idx] != 9 {
+			digits[idx]++
+			break
+		}
+		digits[idx] = 0
+		if idx == 0 {
+			digits = append([]int{1}, digits...)
+			break
+		}
+		idx--
+	}
+	return digits
+}
+
+// @lc code=end
